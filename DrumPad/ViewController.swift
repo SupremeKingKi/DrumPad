00f536ddcac3c952697540eaec568b9d11174549
Kireeti Singam Setty <201418@s020rmc148-16.local>
2019-05-02 10:06:13 -0500

    all

--- DrumPad/ViewController.swift
+++ DrumPad/ViewController.swift
@@ -7,14 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var audioPlayer: AVAudioPlayer?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func SquareOne(_ sender: Any) {
+        playSound(soundFileName: "Basic_Rock_135")
+    }
+    
+    @IBAction func SquareTwo(_ sender: Any) {
+        playSound(soundFileName: "Drum1")
+    }
+    
+    @IBAction func SquareThree(_ sender: Any) {
+        playSound(soundFileName: "Drum3")
+    }
+    
+    @IBAction func SquareFour(_ sender: Any) {
+        playSound(soundFileName: "Drum5")
+    }
+    
+    @IBAction func SquareFive(_ sender: Any) {
+        playSound(soundFileName: "Drum7")
+    }
+    
+    @IBAction func SquareSix(_ sender: Any) {
+        playSound(soundFileName: "Drum9")
+    }
+    
+    @IBAction func SquareSeven(_ sender: Any) {
+        playSound(soundFileName: "Drum11")
+    }
+    
+    @IBAction func SquareEight(_ sender: Any) {
+        playSound(soundFileName: "Drum13")
+    }
+    
+    @IBAction func SquareNine(_ sender: Any) {
+        playSound(soundFileName: "Drum15")
+    }
+    
+    func playSound(soundFileName : String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "mp3")
 
-
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        audioPlayer?.play()
+    }
 }
 
